ed081acfec3fbd37cbef26f131ece93e67748489
John (Jack) Brown <jack@brownjohnf.com>
2016-07-13 14:29:34 -0700

    docs(api): Add docs about how to use the API; Add RESTful routes
    This is step 1 towards migrating to better routing. The GET-based routes
    were originally used to support local testing.

--- src/resinci/server.rb
+++ src/resinci/server.rb
@@ -1,18 +1,66 @@
 module Resinci
   class Server < Sinatra::Base
+    get "/" do
+      content_type "text/plain"
+
+      <<HEREDOC
+Usage:
+
+  GET     /
+    Display this help output
+
+  GET     /containers
+    List running docker-in-docker containers
+
+  POST    /containers
+    Create a new docker-in-docker container
+
+  GET     /dockers/:id
+    Get information about a running container
+
+  GET     /dockers/:id/port
+    Get the port for a particular containerized Docker daemon
+
+  PUT     /dockers/:id/stop
+    Stop a containerized Docker daemon
+HEREDOC
+    end
+
     get "/containers" do
       content_type "text/plain"
 
       Docker.ps
     end
 
-    delete "/containers" do
+    # TODO: remove this route
+    get "/dockers/create" do
       content_type "text/plain"
 
-      Docker.rm_all
+      unless params[:context]
+        return 400, { error: "context is required" }.to_json
+      end
+
+      version = params[:version] || DEFAULT_DOCKER_VERSION
+      context = "%s-%s" % [ params[:context], version]
+
+      if context && context != ""
+        container = Docker.inspect(context)
+        if container
+          unless container.running?
+            container.start
+          end
+
+          return container.connection_string
+        end
+      end
+
+      name = (context && context != "") ? "--name=#{context}" : ""
+      container = Docker.run(name: name, version: version)
+
+      container.connection_string
     end
 
-    get "/dockers/create" do
+    post "/containers" do
       content_type "text/plain"
 
       unless params[:context]
@@ -51,7 +99,14 @@ module Resinci
       Docker.inspect(params[:id]).port
     end
 
+    # TODO: remove this route
     get "/dockers/:id/stop" do
+      content_type "text/plain"
+
+      Docker.stop(params[:id])
+    end
+
+    put "/dockers/:id/stop" do
       content_type "text/plain"
 
       Docker.stop(params[:id])
